bdf24faff7a9f9309fcb65e843ee0c42fe0ca8a0
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-06-13 19:20:34 +1200

    csaacs

--- code/start.gd
+++ code/start.gd
@@ -8,7 +8,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_pressed('ui_f'):
+		get_tree().change_scene_to_file("res://scenes/pinball.tscn") 
 
 
 func _on_button_pressed() -> void:
